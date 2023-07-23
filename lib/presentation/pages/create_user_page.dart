e83a3d565905f6d3bb7e7a8e6b2a374c0d2f852e
Farhan <kuuhaku.1332@gmail.com>
2023-07-24 01:40:05 +0700

    edit: textfield widget

--- lib/presentation/pages/create_user_page.dart
+++ lib/presentation/pages/create_user_page.dart
@@ -15,12 +15,25 @@ class _CreateUserPageState extends State<CreateUserPage> {
   final passwordController = TextEditingController();
   final phoneController = TextEditingController();
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final FocusNode _focusNode = FocusNode();
+  bool _isFocused = false;
+
   @override
   void initState() {
     super.initState();
-    phoneController.addListener(() {
+    fullNameController.addListener(() {
       setState(() {});
     });
+    _focusNode
+        .addListener(() => setState(() => _isFocused = _focusNode.hasFocus));
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
   }
 
   @override
@@ -56,58 +69,110 @@ class _CreateUserPageState extends State<CreateUserPage> {
       ),
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text(
-              'Nama Lengkap',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: CColors.primary,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'Nama Lengkap',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: CColors.primary,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            TextField(
-              controller: phoneController,
-              keyboardType: TextInputType.name,
-              textInputAction: TextInputAction.next,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                color: CColors.primary,
+              const SizedBox(
+                height: 8,
               ),
-              decoration: InputDecoration(
-                suffixIcon: phoneController.text.isEmpty
-                    ? Container(width: 0)
-                    : IconButton(
-                        icon: const Icon(
-                          Icons.close,
-                          color: Colors.amberAccent,
-                        ),
-                        onPressed: () => phoneController.clear(),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  color: CColors.neutral0,
+                  boxShadow: _isFocused
+                      ? [
+                          const BoxShadow(
+                            color: CColors.primary,
+                            blurRadius: 5,
+                            spreadRadius: 2,
+                          ),
+                        ]
+                      : [],
+                ),
+                child: TextFormField(
+                  focusNode: _focusNode,
+                  controller: fullNameController,
+                  keyboardType: TextInputType.name,
+                  textInputAction: TextInputAction.next,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: CColors.primary,
+                  ),
+                  decoration: InputDecoration(
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: const BorderSide(
+                        color: CColors.negative,
+                        width: 2,
                       ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(11),
-                  borderSide: const BorderSide(
-                    color: CColors.primary,
-                    width: 2,
+                    ),
+                    suffixIcon: fullNameController.text.isEmpty
+                        ? Container(width: 0)
+                        : IconButton(
+                            icon: const Icon(
+                              Icons.close,
+                              color: Colors.amberAccent,
+                            ),
+                            onPressed: () => fullNameController.clear(),
+                          ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: const BorderSide(
+                        color: CColors.primary,
+                        width: 2,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: CColors.primary,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(11),
-                  borderSide: const BorderSide(
-                    width: 2,
-                    color: CColors.primary,
-                    style: BorderStyle.solid,
-                  ),
+                  cursorColor: CColors.primary,
+                  validator: (value) {
+                    // if (_isFocused == true) {
+                    //   return null;
+                    // }
+                    if (value!.isEmpty) {
+                      return "text tidak boleh kosong";
+                    }
+                    if (value.length < 5) {
+                      return "text tidak boleh kurang dari 5";
+                    }
+
+                    return null;
+                  },
+                  onFieldSubmitted: (value) {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  onTap: () => _formKey.currentState!.reset(),
                 ),
               ),
-              cursorColor: CColors.primary,
-            ),
-          ],
+              // ElevatedButton(
+              //   onPressed: () {
+              //     // Trigger form validation
+              //     if (_formKey.currentState!.validate()) {
+              //       // All the TextFormField inputs are valid
+              //     }
+              //   },
+              //   child: Text('Submit'),
+              // ),
+            ],
+          ),
         ),
       ),
     );
