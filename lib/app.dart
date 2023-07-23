2ba95a8c54351892aebabecf5900bb0c9aeb344b
Farhan <kuuhaku.1332@gmail.com>
2023-07-23 11:59:57 +0700

    add: login page

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:bank_sampah/presentation/pages/home_page.dart';
+import 'package:bank_sampah/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const LoginPage(),
     );
   }
 }
