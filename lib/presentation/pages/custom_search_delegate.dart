c50553cda2875570ffa25c49313cdbdbe3f01bf7
Farhan <kuuhaku.1332@gmail.com>
2023-08-12 14:24:44 +0700

    edit: icon navbar

---
+++ lib/presentation/pages/custom_search_delegate.dart
@@ -0,0 +1,60 @@
+import 'package:bank_sampah/core/constant/constant_data.dart';
+import 'package:flutter/material.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<String> dummyMonths = ConstantData.months;
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear_rounded),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(
+        Icons.arrow_back_ios_new_rounded,
+      ),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var month in dummyMonths) {
+      if (month.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(month);
+      }
+    }
+
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) => ListTile(title: Text(matchQuery[index])),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var month in dummyMonths) {
+      if (month.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(month);
+      }
+    }
+
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) => ListTile(title: Text(matchQuery[index])),
+    );
+  }
+}
