01b929a501b86a8128ced02cba1dc9803eb88b33
Farhan <kuuhaku.1332@gmail.com>
2023-08-30 00:55:03 +0700

    fix: context.pop bug

--- lib/presentation/pages/admin/admin_create_user_page.dart
+++ lib/presentation/pages/admin/admin_create_user_page.dart
@@ -29,213 +29,218 @@ class AdminCreateUserPage extends StatelessWidget {
       ),
       body: BlocProvider(
         create: (context) => getIt<CreateUserFormBloc>(),
-        child: Form(
-          key: formKey,
-          child: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            children: <Widget>[
-              Builder(
-                builder: (context) {
-                  final state = context.watch<CreateUserFormBloc>().state;
-                  return PhoneField(
-                    suffixIcon: state.isPhoneNumberLoading
-                        ? Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Transform.scale(
-                              scale: 0.7,
-                              child: const CircularProgressIndicator(),
-                            ),
-                          )
-                        : state.isPhoneNumberExists
-                            ? const Icon(Icons.error)
-                            : null,
-                    onChanged: (value) {
-                      context.read<CreateUserFormBloc>().add(CreateUserFormEvent.phoneNumberChanged(value));
-                    },
-                    validator: (_) {
-                      return state.phoneNumber.fold(
-                        (failure) => failure.message,
-                        (_) => null,
-                      );
-                    },
-                  );
+        child: BlocListener<CreateUserFormBloc, CreateUserFormState>(
+          listenWhen: (previous, current) => previous.isSubmitting != current.isSubmitting,
+          listener: (context, state) {
+            state.failureOrSuccessOption.fold(
+              () => null,
+              (failureOrSuccess) => failureOrSuccess.fold(
+                (failure) => FlushbarHelper.createError(message: 'Terjadi kesalahan').show(context),
+                (_) {
+                  context.pop();
                 },
               ),
-              const SizedBox(height: 16),
-              BlocBuilder<CreateUserFormBloc, CreateUserFormState>(
-                buildWhen: (previous, current) => previous.errorMessagesShown != current.errorMessagesShown,
-                builder: (context, state) {
-                  return NameField(
-                    onChanged: (value) {
-                      context.read<CreateUserFormBloc>().add(CreateUserFormEvent.fullNameChanged(value));
-                    },
-                    validator: (_) {
-                      return context.read<CreateUserFormBloc>().state.fullName.fold(
-                            (failure) => failure.message,
-                            (_) => null,
-                          );
-                    },
+            );
+          },
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              children: <Widget>[
+                Builder(
+                  builder: (context) {
+                    final state = context.watch<CreateUserFormBloc>().state;
+                    return PhoneField(
+                      suffixIcon: state.isPhoneNumberLoading
+                          ? Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Transform.scale(
+                                scale: 0.7,
+                                child: const CircularProgressIndicator(),
+                              ),
+                            )
+                          : state.isPhoneNumberExists
+                              ? const Icon(Icons.error)
+                              : null,
+                      onChanged: (value) {
+                        context.read<CreateUserFormBloc>().add(CreateUserFormEvent.phoneNumberChanged(value));
+                      },
+                      validator: (_) {
+                        return state.phoneNumber.fold(
+                          (failure) => failure.message,
+                          (_) => null,
+                        );
+                      },
+                    );
+                  },
+                ),
+                const SizedBox(height: 16),
+                BlocBuilder<CreateUserFormBloc, CreateUserFormState>(
+                  buildWhen: (previous, current) => previous.errorMessagesShown != current.errorMessagesShown,
+                  builder: (context, state) {
+                    return NameField(
+                      onChanged: (value) {
+                        context.read<CreateUserFormBloc>().add(CreateUserFormEvent.fullNameChanged(value));
+                      },
+                      validator: (_) {
+                        return context.read<CreateUserFormBloc>().state.fullName.fold(
+                              (failure) => failure.message,
+                              (_) => null,
+                            );
+                      },
+                    );
+                  },
+                ),
+                Builder(builder: (context) {
+                  return DropdownVillage(
+                    onChanged: (village) =>
+                        context.read<CreateUserFormBloc>().add(CreateUserFormEvent.villageChanged(village)),
                   );
-                },
-              ),
-              Builder(builder: (context) {
-                return DropdownVillage(
-                  onChanged: (village) =>
-                      context.read<CreateUserFormBloc>().add(CreateUserFormEvent.villageChanged(village)),
-                );
-              }),
-              const SizedBox(height: 20),
-              BlocBuilder<CreateUserFormBloc, CreateUserFormState>(
-                builder: (context, state) {
-                  return PasswordField(
-                    onChanged: (value) {
-                      context.read<CreateUserFormBloc>().add(CreateUserFormEvent.passwordChanged(value));
-                    },
-                    validator: (_) {
-                      return context.read<CreateUserFormBloc>().state.password.fold(
-                            (failure) => failure.message,
-                            (_) => null,
-                          );
-                    },
-                  );
-                },
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: (MediaQuery.of(context).size.width / 2) - 25,
-                    child: Builder(builder: (context) {
-                      return RtrwField(
-                        label: 'RT',
-                        hintText: '005',
-                        helperText: '',
-                        onChanged: (value) {
-                          context.read<CreateUserFormBloc>().add(CreateUserFormEvent.rtChanged(value));
-                        },
-                      );
-                    }),
-                  ),
-                  const SizedBox(width: 10),
-                  SizedBox(
-                    width: (MediaQuery.of(context).size.width / 2) - 25,
-                    child: Builder(builder: (context) {
-                      return RtrwField(
-                        label: 'RW',
-                        hintText: '007',
-                        helperText: '',
-                        onChanged: (value) {
-                          context.read<CreateUserFormBloc>().add(CreateUserFormEvent.rwChanged(value));
-                        },
-                      );
-                    }),
-                  ),
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Foto  Pengguna',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      BlocBuilder<CreateUserFormBloc, CreateUserFormState>(
-                        builder: (context, state) {
-                          return UploadPhoto(
-                            file: state.profilePictureOption,
-                            onTap: () =>
-                                context.read<CreateUserFormBloc>().add(const CreateUserFormEvent.imagePickerOpened()),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                  Container(
-                    color: Theme.of(context).colorScheme.primary,
-                    height: 188,
-                    width: 1,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Role',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Builder(builder: (context) {
-                        return RoleChoiceChip(
-                          onSelected: (selectedRole) {
-                            context.read<CreateUserFormBloc>().add(CreateUserFormEvent.roleChanged(selectedRole));
+                }),
+                const SizedBox(height: 20),
+                BlocBuilder<CreateUserFormBloc, CreateUserFormState>(
+                  builder: (context, state) {
+                    return PasswordField(
+                      onChanged: (value) {
+                        context.read<CreateUserFormBloc>().add(CreateUserFormEvent.passwordChanged(value));
+                      },
+                      validator: (_) {
+                        return context.read<CreateUserFormBloc>().state.password.fold(
+                              (failure) => failure.message,
+                              (_) => null,
+                            );
+                      },
+                    );
+                  },
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: (MediaQuery.of(context).size.width / 2) - 25,
+                      child: Builder(builder: (context) {
+                        return RtrwField(
+                          label: 'RT',
+                          hintText: '005',
+                          helperText: '',
+                          onChanged: (value) {
+                            context.read<CreateUserFormBloc>().add(CreateUserFormEvent.rtChanged(value));
                           },
                         );
                       }),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Divider(color: Theme.of(context).colorScheme.primary, height: 1),
-              const SizedBox(height: 20),
-              BlocBuilder<CreateUserFormBloc, CreateUserFormState>(
-                buildWhen: (previous, current) => previous.isSubmitting != current.isSubmitting,
-                builder: (context, state) {
-                  state.failureOrSuccessOption.fold(
-                    () => null,
-                    (failureOrSuccess) => failureOrSuccess.fold(
-                      (failure) => FlushbarHelper.createError(message: 'Terjadi kesalahan').show(context),
-                      (_) {
-                        context.pop();
-                      },
                     ),
-                  );
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        height: 44,
-                        width: (MediaQuery.of(context).size.width / 2) - 25,
-                        child: RoundedButton(
-                          name: "Batal",
-                          onPressed: () => Navigator.pop(context),
-                          color: MyTheme.isDarkMode ? CColors.backgorundDark : CColors.backgorundLight,
-                          selected: false,
-                          textColor: Theme.of(context).colorScheme.primary,
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: (MediaQuery.of(context).size.width / 2) - 25,
+                      child: Builder(builder: (context) {
+                        return RtrwField(
+                          label: 'RW',
+                          hintText: '007',
+                          helperText: '',
+                          onChanged: (value) {
+                            context.read<CreateUserFormBloc>().add(CreateUserFormEvent.rwChanged(value));
+                          },
+                        );
+                      }),
+                    ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Foto  Pengguna',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 44,
-                        width: (MediaQuery.of(context).size.width / 2) - 25,
-                        child: RoundedPrimaryButton(
-                          isLoading: state.isSubmitting,
-                          buttonName: "Simpan",
-                          onPressed: () {
-                            // Validate returns true if the form is valid, or false otherwise.
-                            if (formKey.currentState!.validate()) {
-                              context.read<CreateUserFormBloc>().add(const CreateUserFormEvent.submitButtonPressed());
-                            }
+                        const SizedBox(height: 10),
+                        BlocBuilder<CreateUserFormBloc, CreateUserFormState>(
+                          builder: (context, state) {
+                            return UploadPhoto(
+                              file: state.profilePictureOption,
+                              onTap: () =>
+                                  context.read<CreateUserFormBloc>().add(const CreateUserFormEvent.imagePickerOpened()),
+                            );
                           },
                         ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-              const SizedBox(height: 16),
-            ],
+                      ],
+                    ),
+                    Container(
+                      color: Theme.of(context).colorScheme.primary,
+                      height: 188,
+                      width: 1,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Role',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Builder(builder: (context) {
+                          return RoleChoiceChip(
+                            onSelected: (selectedRole) {
+                              context.read<CreateUserFormBloc>().add(CreateUserFormEvent.roleChanged(selectedRole));
+                            },
+                          );
+                        }),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Divider(color: Theme.of(context).colorScheme.primary, height: 1),
+                const SizedBox(height: 20),
+                BlocBuilder<CreateUserFormBloc, CreateUserFormState>(
+                  buildWhen: (previous, current) => previous.isSubmitting != current.isSubmitting,
+                  builder: (context, state) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          height: 44,
+                          width: (MediaQuery.of(context).size.width / 2) - 25,
+                          child: RoundedButton(
+                            name: "Batal",
+                            onPressed: () => Navigator.pop(context),
+                            color: MyTheme.isDarkMode ? CColors.backgorundDark : CColors.backgorundLight,
+                            selected: false,
+                            textColor: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 44,
+                          width: (MediaQuery.of(context).size.width / 2) - 25,
+                          child: RoundedPrimaryButton(
+                            isLoading: state.isSubmitting,
+                            buttonName: "Simpan",
+                            onPressed: () {
+                              // Validate returns true if the form is valid, or false otherwise.
+                              if (formKey.currentState!.validate()) {
+                                context.read<CreateUserFormBloc>().add(const CreateUserFormEvent.submitButtonPressed());
+                              }
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
           ),
         ),
       ),
