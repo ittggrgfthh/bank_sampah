75ddbb78d0b040fb55914812dfaa633e78984408
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-03-19 03:15:35 +0700

    chore: add `supabase` to change `firebase` #10

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
@@ -17,6 +18,11 @@ void main() async {
     DeviceOrientation.portraitDown,
   ]);
   await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL'),
+    anonKey: dotenv.get('SUPABASE_KEY'),
+  );
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
